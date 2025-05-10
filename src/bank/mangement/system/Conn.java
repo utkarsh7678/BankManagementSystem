4fdbab5e7187239e5dd99666a1bfed7cfad1d320
utkarsh7678 <us803397@gmail.com>
2025-05-11 01:50:59 +0530

    Added full NetBeans project

---
+++ src/bank/mangement/system/Conn.java
@@ -0,0 +1,20 @@
+package bank.mangement.system;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Us803397@");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
